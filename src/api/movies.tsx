47e5c853525d26b44dc98ead0eb93cb4e3114b65
Jegan Matthews <matthewsjegan@gmail.com>
2020-09-08 10:28:40 -0700

    updated movies

--- src/api/movies.tsx
+++ src/api/movies.tsx
@@ -2,61 +2,61 @@ export const movies = [
   {
     "id": "1",
     "name": "Atlanta - S01 e01 - The Big Bang",
-    "url": "https://media.w3.org/2010/05/sintel/trailer.mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E01 - The Big Bang (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "2",
     "name": "Atlanta - S01 e02 - Streets on Lock",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E02 - Streets on Lock (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "3",
     "name": "Atlanta - S01 e03 - Go for Broke",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E03 - Go for Broke (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "4",
     "name": "Atlanta - S01 e04 - The Streisand Effect",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E04 - The Streisand Effect (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "5",
     "name": "Atlanta - S01 e05 - Nobody Beats the Biebs",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E05 - Nobody Beats the Biebs (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "6",
     "name": "Atlanta - S01 e06 - Value",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E06 - Value (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "7",
     "name": "Atlanta - S01 e07 - B.A.N.",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E07 - B.A.N. aka BLACK American Network (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "8",
     "name": "Atlanta - S01 e08 - The Club",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E08 - The Club (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "9",
     "name": "Atlanta - S01 e09 - Juneteenth",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E09 - Juneteenth (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
   {
     "id": "10",
     "name": "Atlanta - S01 e10 - The Jacket",
-    "url": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/ATLANTA%20-%20S01%20E02%20-%20Streets%20on%20Lock%20(720p%20Web-DL).mp4",
-    "cover": "https://coolkidsvcdb.file.core.windows.net/atlanta-file-share/Atlanta.png"
+    "url": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/ATLANTA - S01 E10 - The Jacket (720p Web-DL).mp4",
+    "cover": "https://coolkidsvideoclub.blob.core.windows.net/atlanta/Atlanta.png"
   },
 ]
